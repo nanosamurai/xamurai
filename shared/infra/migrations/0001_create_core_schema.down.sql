2b59589cdc04c9cdf6a4965482332c10f880aa1f
mikub <miroslav.kubicek@gmail.com>
2025-12-18 11:57:14 +0100

    add initial db schema migration script

---
+++ shared/infra/migrations/0001_create_core_schema.down.sql
@@ -0,0 +1,7 @@
+-- Reverse order of table creation
+DROP TABLE IF EXISTS speakers;
+DROP TABLE IF EXISTS session_transcripts;
+DROP TABLE IF EXISTS recordings;
+DROP TABLE IF EXISTS sessions;
+DROP TABLE IF EXISTS app_users;
+DROP TABLE IF EXISTS tenants;
---
+++ shared/infra/migrations/0001_create_core_schema.py
@@ -0,0 +1,12 @@
+from sql_helpers import run_sql_pair, run_sql_pair_down
+
+revision = "0001_create_core_schema"
+#down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    run_sql_pair(__file__, "0001_create_core_schema")
+
+def downgrade():
+    run_sql_pair_down(__file__, "0001_create_core_schema")
