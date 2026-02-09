f2c4f3adf2f465e08b1e857c45f885f2738cce84
mikub <miroslav.kubicek@gmail.com>
2026-02-09 06:00:52 +0100

    Move persistence to samuraipersistor; remove Python DB/migrations/BFF/tests

--- migrations/versions/0001_create_core_schema.down.sql
+++
@@ -1,7 +0,0 @@
--- Reverse order of table creation
-DROP TABLE IF EXISTS speakers;
-DROP TABLE IF EXISTS session_transcripts;
-DROP TABLE IF EXISTS recordings;
-DROP TABLE IF EXISTS sessions;
-DROP TABLE IF EXISTS app_users;
-DROP TABLE IF EXISTS tenants;
