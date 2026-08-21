cdd758b0d25cc23097cfcf021a9d806967295212
mikub <miroslav.kubicek@gmail.com>
2026-08-21 23:17:05 +0200

    Define the speech provider protocol

--- proto_gen/generate-proto.sh
+++ proto_gen/generate-proto.sh
@@ -1,1 +1,4 @@
-python -m grpc_tools.protoc -I ../proto --python_out=. --grpc_python_out=. ../proto/stream.proto
+python -m grpc_tools.protoc -I ../proto --python_out=. --grpc_python_out=. \
+  ../proto/stream.proto ../proto/speech_provider.proto
+sed -i 's/^import stream_pb2/from . import stream_pb2/' stream_pb2_grpc.py
+sed -i 's/^import speech_provider_pb2/from . import speech_provider_pb2/' speech_provider_pb2_grpc.py
